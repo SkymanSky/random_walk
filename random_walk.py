2529684e777a1a4389e276dff475e6804d1ef210
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-12 21:57:35 +0300

    fill_walk metodu tanımlandı.Yürüyüşün adımları hesaplandı.

--- random_walk.py
+++ random_walk.py
@@ -11,4 +11,28 @@ class RandomWalk:
         self.x_values=[0]
         self.y_values=[0]
 
+    def fill_walk(self):
+        """Yürüyüşteki bütün noktaları hesapla."""
+        #Yürüyüş istenen uzunluğa ulaşana kadar adım atmaya devam et.
+        while len(self.x_values)<self.num_points:
+            #Hangi yöne gidileceğine ve bu yönde ne kadar uzağa gidileceğine
+            #kara ver.
+            x_direction=choice([1,-1])
+            x_distance=choice([0,1,2,3,4])
+            x_step=x_direction*x_distance
+
+            y_direction=choice([1,-1])
+            y_distance=choice([0,1,2,3,4])
+            y_step=y_direction*y_distance
+
+            #Hiçbir yere gitmeyen hareketleri redded.
+            if x_step==0 and y_step==0:
+                continue
+            #Yeni konumu hesapla
+            x=self.x_values[-1]+x_step
+            y=self.y_values[-1]+y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
         
