66474ca7d53b96a45aa717f6a9d8444237476804
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-13 22:22:24 +0300

    Grafik nokta sayısı 50000 olarak güncellendi.

--- rw_visual.py
+++ rw_visual.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 #Program aktif olduğu sürece yeni yürüyüşler yapmayı sürdür.
 while True:
-    rw=RandomWalk()
+    rw=RandomWalk(50000)
     rw.fill_walk()
     #Yürüyüşteki noktaları çiz.
     plt.style.use('classic')
     fig, ax= plt.subplots()
     point_numbers=range(rw.num_points)
-    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none',s=15)
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none',s=1)
     #İlk ve son noktaları vurgula.
     ax.scatter(0,0,c='green',edgecolors='none',s=100)
     ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',s=100,edgecolors='none')
