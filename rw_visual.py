531d4df31003913838a285f8f2f01ac2b46cd195
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-13 21:56:55 +0300

    Birden çok rastgele yürüyüş için while döngüsü yazıldı.

--- rw_visual.py
+++ rw_visual.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
-#Rastgele bir yürüyüş yap.
-rw=RandomWalk()
-rw.fill_walk()
-#Yürüyüşteki noktaları çiz.
-plt.style.use('classic')
-fig, ax= plt.subplots()
-ax.scatter(rw.x_values,rw.y_values,s=15)
-plt.show()
+#Program aktif olduğu sürece yeni yürüyüşler yapmayı sürdür.
+while True:
+    rw=RandomWalk()
+    rw.fill_walk()
+    #Yürüyüşteki noktaları çiz.
+    plt.style.use('classic')
+    fig, ax= plt.subplots()
+    ax.scatter(rw.x_values,rw.y_values,s=15)
+    plt.show()
+
+    keep_running=input("Make another walk? (y/n): ")
+    if keep_running=='n':
+        break
