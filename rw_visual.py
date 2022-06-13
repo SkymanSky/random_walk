010070646c59a2c21a001fe56c61fa043df33478
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-13 22:07:32 +0300

    Başlangıç ve bitiş noktaları vurgulandı.

--- rw_visual.py
+++ rw_visual.py
@@ -9,7 +9,9 @@ while True:
     fig, ax= plt.subplots()
     point_numbers=range(rw.num_points)
     ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none',s=15)
-    #ax.scatter(rw.x_values,rw.y_values,s=15)
+    #İlk ve son noktaları vurgula.
+    ax.scatter(0,0,c='green',edgecolors='none',s=100)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',s=100,edgecolors='none')
     plt.show()
 
     keep_running=input("Make another walk? (y/n): ")
